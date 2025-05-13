cd8a6dab5450ee11af81f9f2088170c0d3340e21
Davis Mero <davismero17@gmail.com>
2025-05-13 10:55:49 -0500

    diseño de la calculadorav.2

---
+++ script.js
@@ -0,0 +1,32 @@
+
+function sumar() {
+var num1 = document.getElementById("num1").value;
+var num2 = document.getElementById("num2").value;
+    var resultado = parseInt(num1) + parseInt(num2);
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+
+}
+function resta() {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+        var resultado = parseInt(num1) - parseInt(num2);
+        document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+    
+    }
+    function multiplica() {
+        var num1 = document.getElementById("num1").value;
+        var num2 = document.getElementById("num2").value;
+            var resultado = parseInt(num1) * parseInt(num2);
+            document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+        
+        }
+        function division() {
+            var num1 = document.getElementById("num1").value;
+            var num2 = document.getElementById("num2").value;
+                var resultado = parseInt(num1) / parseInt(num2);
+                document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+            
+            }
+            
+        
+    
